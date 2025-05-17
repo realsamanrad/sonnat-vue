ba5202272dcc1be117e6e980bc83d993816f085d
realsamanrad <samy.janson@gmail.com>
2025-05-17 15:26:01 +0330

    Refactor SChip for improved component structure and accessibility

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,239 @@
 <template>
   <div class="mr-10 mt-10 flex flex-col gap-6">
-    <SButton color="primary" size="md" variant="inlined" :icon="HeartSVG" />
+    <SButton :icon="HeartSVG" color="primary" size="md" variant="inlined" />
     <SBadge label="جدید" />
-    <STag label="محصولات" :icon="HeartSVG" removable color="success" />
-    <SChip
-      label="گران ترین"
-      behavior="removable"
-      rounded
-      variant="filled"
-      color="primary"
-      :icon="HeartSVG"
-    />
+    <STag :icon="HeartSVG" color="success" label="محصولات" removable />
+    <div class="flex gap-4 flex-wrap mb-12">
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="primary"
+        label="گران ترین"
+        rounded
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="primary"
+        label="گران ترین"
+        rounded
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        label="گران ترین"
+        rounded
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="primary"
+        label="گران ترین"
+        rounded
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="primary"
+        label="گران ترین"
+        rounded
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        label="گران ترین"
+        rounded
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="primary"
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="primary"
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="primary"
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="primary"
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="default"
+        disabled
+        label="ارزان‌ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="interactive"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+
+      <SChip
+        v-model:selected="selected"
+        :icon="HeartSVG"
+        behavior="selective"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        v-model:selected="selected"
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        v-model:selected="selected"
+        :icon="HeartSVG"
+        behavior="selective"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        v-model:selected="selected"
+        :icon="HeartSVG"
+        behavior="selective"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+
+      |
+
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="filled"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="default"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+      <SChip
+        :icon="HeartSVG"
+        behavior="removable"
+        color="primary"
+        disabled
+        label="گران ترین"
+        variant="outlined"
+      />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import SButton from '@/components/SButton.vue'
 import SBadge from '@/components/SBadge.vue'
 import STag from '@/components/STag.vue'
 import SChip from '@/components/SChip.vue'
 import HeartSVG from '@/assets/icons/heart.svg'
+import { ref } from 'vue'
+
+const selected = ref(true)
 </script>
--- src/components/SChip.vue
+++ src/components/SChip.vue
@@ -1,21 +1,17 @@
 <template>
   <div
-    role="button"
     :aria-disabled="disabled"
-    class="rounded-sm flex items-center justify-center w-fit select-none transition duration-300"
     :class="[
-      // _class[variant][color],
       sizeClass,
-
+      _class,
       {
         '!rounded-full': rounded,
         'cursor-pointer': behavior === 'selective' || behavior === 'interactive',
         'pointer-events-none': disabled,
-        [interactiveClass[variant][color]]: behavior === 'interactive',
-        [selectiveClass[selected ? 'true' : 'false'][variant][color]]: behavior === 'selective',
-        [removableClass[variant][color]]: behavior === 'removable',
       },
     ]"
+    class="rounded-sm flex items-center justify-center w-fit select-none transition duration-300"
+    role="button"
     @click="handleClick"
   >
     <inline-svg v-if="icon" :src="icon" />
@@ -27,15 +23,15 @@
       class="h-full group cursor-pointer flex justify-center items-center"
     >
       <inline-svg
-        class="size-4 rounded-full transition duration-300"
-        :class="closeBtnClass"
+        :class="btnStateClass[variant]"
         :src="CloseSVG"
+        class="size-4 rounded-full transition duration-300"
       />
     </button>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { computed } from 'vue'
 import CloseSVG from '@/assets/icons/close.svg'
 
@@ -51,19 +47,14 @@ const {
   variant?: 'filled' | 'outlined'
   color?: 'primary' | 'default'
   rounded?: boolean
-  trailingIcon?: string
   icon?: string
   size?: 'sm' | 'md' | 'lg'
   disabled?: boolean
   behavior?: 'removable' | 'selective' | 'interactive'
 }>()
 
-const closeBtnClass = computed(() => {
-  const colorMap: Record<string, string> = {
-    default: 'group-hover:bg-black/12 group-active:bg-black/24',
-    primary: 'group-hover:bg-primary/12 group-active:bg-primary/24',
-  }
-  return colorMap[color] || ''
+const selected = defineModel<boolean>('selected', {
+  default: false,
 })
 
 const sizeClass = computed(() => {
@@ -74,55 +65,78 @@ const sizeClass = computed(() => {
   }[size]
 })
 
+const btnStateClass = {
+  filled: 'group-hover:bg-black/12 group-active:bg-black/24',
+  outlined: 'group-hover:bg-primary/12 group-active:bg-primary/24',
+}
+
 const interactiveClass = {
   filled: {
-    default: 'text-black/56 bg-black/4 hover:bg-black/12 active:bg-black/24',
-    primary: 'text-white bg-primary hover:bg-primary-400 active:bg-primary-600',
+    default:
+      'text-black/56 bg-black/4 hover:bg-black/12 active:bg-black/24 aria-disabled:bg-black/4 aria-disabled:text-black/24',
+    primary:
+      'text-white bg-primary hover:bg-primary-400 active:bg-primary-600 aria-disabled:bg-primary/8 aria-disabled:text-black/24',
   },
   outlined: {
-    default: 'text-black/56 border border-black/12 hover:bg-black/4 active:bg-black/12',
-    primary: 'text-primary border border-primary hover:bg-primary/12 active:bg-primary/24',
+    default:
+      'text-black/56 border border-black/12 hover:bg-black/4 active:bg-black/12 aria-disabled:border-black/12 aria-disabled:text-black/24',
+    primary:
+      'text-primary border border-primary hover:bg-primary/12 active:bg-primary/24 aria-disabled:border-primary/12 aria-disabled:text-primary/32',
   },
 }
 
 const selectiveClass = {
   true: {
     filled: {
-      default: 'text-white bg-black/56 hover:black/48 active:!bg-black/64',
-      primary: 'text-white bg-primary hover:bg-primary-400 active:bg-primary-600',
+      default:
+        'text-white bg-black/56 hover:black/48 active:!bg-black/64 aria-disabled:bg-black/4 aria-disabled:text-black/24',
+      primary:
+        'text-white bg-primary hover:bg-primary-400 active:bg-primary-600 aria-disabled:bg-primary/8 aria-disabled:text-black/24',
     },
     outlined: {
       default:
-        'text-black/56 border border-black/48 bg-black/4 hover:bg-black/12 active:bg-black/24',
+        'text-black/56 border border-black/48 bg-black/4 hover:bg-black/12 active:bg-black/24 aria-disabled:bg-black/4 aria-disabled:text-black/24 aria-disabled:border-transparent',
       primary:
-        'text-primary border border-primary bg-primary/4 hover:bg-primary/12 active:bg-primary/24',
+        'text-primary border border-primary bg-primary/8 hover:bg-primary/12 active:bg-primary/24 aria-disabled:bg-primary/8 aria-disabled:text-black/24 aria-disabled:border-transparent',
     },
   },
   false: {
     filled: {
-      default: 'text-black/56 bg-black/4 hover:bg-black/12 active:bg-black/24',
-      primary: 'text-black/56 bg-black/4 hover:bg-black/12 active:bg-black/24',
+      default:
+        'text-black/56 bg-black/4 hover:bg-black/12 active:bg-black/24 aria-disabled:bg-black/4 aria-disabled:text-black/24',
+      primary:
+        'text-black/56 bg-black/4 hover:bg-black/12 active:bg-black/24 aria-disabled:bg-primary/8 aria-disabled:text-primary/32',
     },
     outlined: {
-      default: 'text-black/56 border border-black/12 hover:bg-black/4 active:bg-black/12',
-      primary: 'text-black/56 border border-black/12 hover:bg-black/4 active:bg-black/12',
+      default:
+        'text-black/56 border border-black/12 hover:bg-black/4 active:bg-black/12 aria-disabled:border-black/12 aria-disabled:text-black/24',
+      primary:
+        'text-black/56 border border-black/12 hover:bg-black/4 active:bg-black/12 aria-disabled:border-primary/12 aria-disabled:text-primary/32',
     },
   },
 }
 
 const removableClass = {
   filled: {
-    default: 'text-black/56 bg-black/4',
-    primary: 'text-white bg-primary',
+    default: 'text-black/56 bg-black/4 aria-disabled:text-black/24 aria-disabled:bg-black/4',
+    primary: 'text-white bg-primary aria-disabled:text-black/24 aria-disabled:bg-primary/8',
   },
   outlined: {
-    default: 'text-black/56 border border-black/12 bg-black/4',
-    primary: 'text-primary border border-primary bg-primary/4',
+    default:
+      'text-black/56 border border-black/12 bg-black/4 aria-disabled:text-black/24 aria-disabled:border-black/12 aria-disabled:bg-transparent',
+    primary:
+      'text-primary border border-primary bg-primary/8 aria-disabled:text-primary/32 aria-disabled:border-primary/12 aria-disabled:bg-transparent',
   },
 }
 
-const selected = defineModel<boolean>('selected', {
-  default: false,
+const _class = computed(() => {
+  if (behavior === 'interactive') {
+    return interactiveClass[variant][color]
+  }
+  if (behavior === 'selective') {
+    return selectiveClass[selected.value ? 'true' : 'false'][variant][color]
+  }
+  return removableClass[variant][color]
 })
 
 function handleClick() {
