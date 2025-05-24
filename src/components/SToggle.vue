91eec75ba0fc166553f7d753a87f3f29da9ab2d0
realsamanrad <samy.janson@gmail.com>
2025-05-24 12:26:19 +0330

    Add SToggle component for toggle switch functionality and styling

---
+++ src/components/SToggle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="sizeClass">
+    <label :for="name" v-text="label" />
+    <div class="px-2">
+      <input :name type="checkbox" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const { disabled = false, size = 'md' } = defineProps<{
+  disabled?: boolean
+  size?: 'sm' | 'md' | 'lg'
+  label?: string
+  name: string
+}>()
+
+const sizeClass = computed(() => {
+  return {
+    sm: 's-btn-sm',
+    md: 's-btn-md',
+    lg: 's-btn-lg',
+  }[size]
+})
+</script>
