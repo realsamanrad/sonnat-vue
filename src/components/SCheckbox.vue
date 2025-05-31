b5fd450e3dd5b0b5a0dc9e095c153e0db22fe0fb
realsamanrad <samy.janson@gmail.com>
2025-05-31 09:24:18 +0330

    Enhance SCheckbox and SRadio components for improved accessibility and styling; update input structure and visual feedback

--- src/components/SCheckbox.vue
+++ src/components/SCheckbox.vue
@@ -1,18 +1,31 @@
 <template>
-  <div :class="sizeClass" class="flex items-center" role="button" :aria-disabled="disabled">
-    <div class="flex items-center rounded-full bg-black-8 justify-center">
-      <input type="checkbox" :id="inputId" class="size-full opacity-0 cursor-pointer peer" />
+  <div
+    :aria-disabled="disabled"
+    :class="sizeClass"
+    class="inline-flex items-center gap-1"
+    role="button"
+  >
+    <div class="inline-flex items-center justify-center">
+      <input
+        :id="inputId"
+        :disabled
+        class="relative before:size-full appearance-none cursor-pointer peer before:rounded-full before:bg-black/4 checked:before:bg-primary/4 before:opacity-0 before:absolute before:scale-0 before:transition before:duration-240 focus-visible:not-hover:before:bg-black/12 focus-visible:not-hover:checked:before:bg-primary/12 focus-visible:outline-0 focus-visible:not-hover:before:scale-100 focus-visible:before:opacity-100 disabled:pointer-events-none hover:before:scale-100 hover:before:opacity-100 group"
+        type="checkbox"
+      />
       <div
-        class="size-4.5 rounded-xs ring ring-black-24 ring-inset absolute pointer-events-none peer-checked:bg-primary peer-checked:ring-0 flex items-center justify-center peer-not-checked:[&>svg]:hidden"
+        class="rounded-xs ring ring-black-24 ring-inset absolute _pointer-events-none peer-checked:bg-primary peer-checked:ring-0 flex items-center justify-center peer-not-checked:[&>svg]:hidden peer-disabled:peer-checked:bg-black/12 peer-disabled:ring-black/12"
       >
-        <inline-svg :src="CheckSVG" class="size-6 text-white" />
+        <inline-svg
+          :src="CheckSVG"
+          class="text-white rotate-45 transition-all duration-1000 peer-checked:rotate-0"
+        />
       </div>
     </div>
-    <label v-text="label" :for="inputId" />
+    <label :for="inputId" class="text-black-87" v-text="label" />
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { computed, useId } from 'vue'
 import CheckSVG from '@/assets/icons/check.svg'
 
--- src/components/SRadio.vue
+++ src/components/SRadio.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex items-center" :class="sizeClass" role="button" :aria-disabled="disabled">
+  <div :aria-disabled="disabled" :class="sizeClass" class="flex items-center" role="button">
     <div
       class="group size-8 rounded-full ml-1 cursor-pointer relative flex items-center justify-center"
     >
       <input
-        class="peer size-full cursor-pointer _appearance-none before:size-full before:bg-black/4 before:absolute before:rounded-full appearance-none before:opacity-0 before:scale-0 group-hover:before:scale-100 group-hover:before:opacity-100 before:transition checked:before:bg-primary/4 before:duration-240 focus-visible:before:bg-black/12 checked:focus-visible:before:bg-primary/12 focus-visible:before:opacity-100 focus-visible:before:scale-100 focus-visible:outline-0"
-        type="radio"
         :id="inputId"
+        class="peer size-full cursor-pointer before:size-full before:bg-black/4 before:absolute before:rounded-full appearance-none before:opacity-0 before:scale-0 group-hover:before:scale-100 group-hover:before:opacity-100 before:transition checked:before:bg-primary/4 before:duration-240 focus-visible:before:bg-black/12 checked:focus-visible:before:bg-primary/12 focus-visible:before:opacity-100 focus-visible:before:scale-100 focus-visible:outline-0"
+        type="radio"
       />
       <span
         class="size-4 rounded-full border border-black/24 absolute peer-checked:border-primary/32 pointer-events-none after:bg-primary after:size-2 after:absolute after:rounded-full flex items-center justify-center after:scale-0 after:opacity-0 peer-checked:after:scale-100 peer-checked:after:opacity-100 after:transition after:duration-240"
@@ -16,7 +16,7 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { computed, useId } from 'vue'
 
 const inputId = 'radio-' + useId()
