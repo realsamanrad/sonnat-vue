3af89ac1f4524af52cf978f6ed535fb93e183fc4
realsamanrad <samy.janson@gmail.com>
2025-06-01 09:43:32 +0330

    Remove `examples`

--- examples/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import SonnatVue from '../src'
-import '../src/styles/index.css'
-
-const app = createApp(App)
-app.use(SonnatVue)
-app.mount('#app') 
